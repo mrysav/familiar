832be0524b5a942fc6beb06098a07ad64d41e067
Mitchell Rysavy <mitchell.rysavy@gmail.com>
2015-06-17 18:40:14 -0500

    Changes to deploy and secrets config

--- config/deploy.rb
+++ config/deploy.rb
@@ -24,7 +24,7 @@ set :pty, true
 
 # Default value for :linked_files is []
 # set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
-set :linked_files, fetch(:linked_files, []).push('config/secrets.yml')
+set :linked_files, fetch(:linked_files, []).push('.env', 'config/secrets.yml')
 
 # Default value for linked_dirs is []
 # set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
@@ -52,6 +52,8 @@ namespace :deploy do
         invoke 'unicorn:restart'
       end
     end
+
+  after :publishing, :restart
 
   after :restart, :clear_cache do
     on roles(:web), in: :groups, limit: 3, wait: 10 do

040c0c94f5013530b9171d12be4bda5dc06bb434
Mitchell Rysavy <mitchell.rysavy@gmail.com>
2015-07-22 17:19:29 -0500

    Made searches fuzzier

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150722194859) do
+ActiveRecord::Schema.define(version: 20150722221351) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "pg_trgm"
+  enable_extension "fuzzystrmatch"
 
   create_table "people", force: :cascade do |t|
     t.string   "name"

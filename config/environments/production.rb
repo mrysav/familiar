896701afbf1e85c0313e5af8b9e5943c524b0ff8
Mitchell Rysavy <mitchell.rysavy@gmail.com>
2015-11-16 10:53:35 -0600

    Fix config formatting?

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,7 +81,7 @@ Rails.application.configure do
     :storage => :s3,
     :s3_credentials => {
       :bucket => ENV['S3_BUCKET_NAME'],
-      :url => :s3_alias_url,
+      :url => ':s3_alias_url',
       :s3_host_alias => ENV['S3_HOST_ALIAS'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']

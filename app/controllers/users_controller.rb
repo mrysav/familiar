097e7aaf474607f13db9e96fe2d4a17a46dc2e6e
Mitchell Rysavy <mitchell.rysavy@gmail.com>
2016-01-03 17:00:01 -0600

    Apply sorting to the user list for admins

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,6 @@ class UsersController < ApplicationController
     before_filter :require_editor, only: [:manage]
     
     def manage
-        @users = User.all.paginate(:page => params[:page], :per_page => 50)
+        @users = User.all.order('editor ASC, name').paginate(:page => params[:page], :per_page => 50)
     end
 end

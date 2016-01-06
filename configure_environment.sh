ad8faab33d5a48c50bb0fbf2c27b2e9d32a7e762
Mitchell Rysavy <mitchell.rysavy@gmail.com>
2016-01-05 22:53:42 -0600

    Updated configure_environment.sh

--- configure_environment.sh
+++ configure_environment.sh
@@ -2,6 +2,8 @@
 
 if [ -e "./.env" ]; then source '.env'; fi
 
+echo "If you do not want to use Facebook authentication, please leave the following blank."
+
 if [ -z ${FACEBOOK_ID+x} ]; then
     read -p "Facebook Application ID: " FACEBOOK_ID
 fi
@@ -10,16 +12,29 @@ if [ -z ${FACEBOOK_SECRET+x} ]; then
     read -p "Facebook Application Secret: " FACEBOOK_SECRET
 fi
 
+echo "If you do not want to use Google authentication, please leave the following blank."
+
+if [ -z ${GOOGLE_ID+x} ]; then
+    read -p "Google Application ID: " GOOGLE_ID
+fi
+
+if [ -z ${GOOGLE_SECRET+x} ]; then
+    read -p "Google Application Secret: " GOOGLE_SECRET
+fi
+
+echo "Amazon S3 is used for photo storage, but only in the production Rails environment."
+echo "If you aren't going to be using familiar in production, or you're using Docker, leave the following blank."
+
 if [ -z ${AWS_ACCESS_KEY_ID+x} ]; then
-    read -p "AWS Access Key ID (only used in 'production' environment): " AWS_ACCESS_KEY_ID
+    read -p "AWS Access Key ID: " AWS_ACCESS_KEY_ID
 fi
 
 if [ -z ${AWS_SECRET_ACCESS_KEY+x} ]; then
-    read -p "AWS Secret Access Key (only used in 'production' environment): " AWS_SECRET_ACCESS_KEY
+    read -p "AWS Secret Access Key: " AWS_SECRET_ACCESS_KEY
 fi
 
 if [ -z ${S3_BUCKET_NAME+x} ]; then
-    read -p "AWS S3 Bucket Name (only used in 'production' environment): " S3_BUCKET_NAME
+    read -p "AWS S3 Bucket Name: " S3_BUCKET_NAME
 fi
 
 if [ -z ${SECRET_KEY_BASE+x} ]; then

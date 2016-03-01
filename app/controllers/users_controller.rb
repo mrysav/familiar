f79fa3b23a07f8d03c1d2855422b79568dbf7808
Mitchell Rysavy <mitchell.rysavy@gmail.com>
2016-03-01 13:20:34 -0600

    Add simple profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,12 @@
 class UsersController < ApplicationController
     before_filter :require_editor, only: [:manage]
+    before_filter :require_valid_user, only: [:profile]
     
     def manage
         @users = User.all.order('editor ASC, name').paginate(:page => params[:page], :per_page => 50)
+    end
+    
+    def profile
+        @user = User.find(current_user.id)
     end
 end
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,10 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, Rails.application.secrets.facebook_id, Rails.application.secrets.facebook_secret
+  provider :facebook, Rails.application.secrets.facebook_id, Rails.application.secrets.facebook_secret, 
+      :image_size => { :width => 200, :height => 200 },
+      :secure_image_url => :true
+      
   provider :google_oauth2, Rails.application.secrets.google_id, Rails.application.secrets.google_secret
+  
   provider :identity, :fields => [:name, :email, :image], on_failed_registration: lambda { |env|    
     SessionsController.action(:register).call(env)
   }

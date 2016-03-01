cf110f4a2cc85cbd67f10399b8db3388d87ecc02
Mitchell Rysavy <mitchell.rysavy@gmail.com>
2016-03-01 13:20:14 -0600

    Change configure_environment.sh again. I'm learning so much bash!

--- configure_environment.sh
+++ configure_environment.sh
@@ -1,46 +1,37 @@
 #!/bin/bash -e
 
-if [ -e "./.env" ]; then source '.env'; fi
+if [ -e ".env" ]; then source '.env'; fi
 
 echo "If you do not want to use Facebook authentication, please leave the following blank."
 
-if [ -z ${FACEBOOK_ID+x} ]; then
-    read -p "Facebook Application ID: " FACEBOOK_ID
-fi
+read -p "Facebook Application ID [$FACEBOOK_ID]: " N_FACEBOOK_ID
+FACEBOOK_ID="${N_FACEBOOK_ID:=$FACEBOOK_ID}"
 
-if [ -z ${FACEBOOK_SECRET+x} ]; then
-    read -p "Facebook Application Secret: " FACEBOOK_SECRET
-fi
+read -p "Facebook Application Secret [$FACEBOOK_SECRET]: " N_FACEBOOK_SECRET
+FACEBOOK_SECRET="${N_FACEBOOK_SECRET:=$FACEBOOK_SECRET}"
 
 echo "If you do not want to use Google authentication, please leave the following blank."
 
-if [ -z ${GOOGLE_ID+x} ]; then
-    read -p "Google Application ID: " GOOGLE_ID
-fi
+read -p "Google Application ID [$GOOGLE_ID]: " N_GOOGLE_ID
+GOOGLE_ID="${N_GOOGLE_ID:=$GOOGLE_ID}"
 
-if [ -z ${GOOGLE_SECRET+x} ]; then
-    read -p "Google Application Secret: " GOOGLE_SECRET
-fi
+read -p "Google Application Secret [$GOOGLE_SECRET]: " N_GOOGLE_SECRET
+GOOGLE_SECRET="${N_GOOGLE_SECRET:=$GOOGLE_SECRET}"
 
 echo "Amazon S3 is used for photo storage, but only in the production Rails environment."
 echo "If you aren't going to be using familiar in production, or you're using Docker, leave the following blank."
 
-if [ -z ${AWS_ACCESS_KEY_ID+x} ]; then
-    read -p "AWS Access Key ID: " AWS_ACCESS_KEY_ID
-fi
+read -p "AWS Access Key ID [$AWS_ACCESS_KEY_ID]: " N_AWS_ACCESS_KEY_ID
+AWS_ACCESS_KEY_ID="${N_AWS_ACCESS_KEY_ID:=$AWS_ACCESS_KEY_ID}"
 
-if [ -z ${AWS_SECRET_ACCESS_KEY+x} ]; then
-    read -p "AWS Secret Access Key: " AWS_SECRET_ACCESS_KEY
-fi
+read -p "AWS Secret Access Key [$AWS_SECRET_ACCESS_KEY]: " N_AWS_SECRET_ACCESS_KEY
+AWS_SECRET_ACCESS_KEY="${N_AWS_SECRET_ACCESS_KEY:=$AWS_SECRET_ACCESS_KEY}"
 
-if [ -z ${S3_BUCKET_NAME+x} ]; then
-    read -p "AWS S3 Bucket Name: " S3_BUCKET_NAME
-fi
+read -p "S3 Bucket Name [$S3_BUCKET_NAME]: " N_S3_BUCKET_NAME
+S3_BUCKET_NAME="${N_S3_BUCKET_NAME:=$S3_BUCKET_NAME}"
 
-if [ -z ${SECRET_KEY_BASE+x} ]; then
-    echo "Generating secret with 'rake secret'..."
-    SECRET_KEY_BASE=`rake secret`
-fi
+echo "Regenerating secret with 'rake secret'..."
+SECRET_KEY_BASE=`rake secret`
 
 if [ -e "./.env" ]; then rm .env; fi
 
@@ -53,4 +44,4 @@ echo "AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY" >> .env
 echo "S3_BUCKET_NAME=$S3_BUCKET_NAME" >> .env
 echo "SECRET_KEY_BASE=$SECRET_KEY_BASE" >> .env
 
-echo "Configuration complete. Please edit .env if you need to change these values."
+echo "Configuration complete. Rerun this script to change values."

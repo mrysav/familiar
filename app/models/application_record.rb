3eaa6b98e0cbde493d3dbe1d69620602fb20005b
Mitchell Rysavy <mitchell.rysavy@gmail.com>
2016-07-15 00:27:58 -0700

    Filter HTML tags in markdown. If there's one thing I learned in CS security...

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -2,6 +2,6 @@ class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
   
   # Initializes a Markdown parser
-  @@markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, autolink: true, tables: true)
+  @@markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML.new(filter_html: true), autolink: true, tables: true)
   
 end
